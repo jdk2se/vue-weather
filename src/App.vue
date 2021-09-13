12c6e5e2b00e7dec95862a844c120cc729baaba5
Dudnik Evgeny <johnydvgu@gmail.com>
2021-09-13 21:03:55 +1000

    finish

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,136 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <main>
+      <div class="search-box">
+        <input type="text" v-model="query" @keypress="fetchWeather" class="search-bar" placeholder="Search...">
+      </div>
+
+      <div class="weather-wrap" v-if="weather.main">
+        <div class="location-box">
+          <div class="location">{{ weather.name }}</div>
+          <div class="date">{{ new Date(1504095567183).toLocaleDateString("en-US") }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
+        </div>
+      </div>
+    </main>
+  </div>
 </template>
 
 <script>
 
 export default {
   name: 'App',
+  data() {
+    return {
+      apiKey: '7acd377ec335ade847c56addcfdd6b78',
+      urlBase: 'https://api.openweathermap.org/data/2.5/',
+      query: '',
+      weather: {},
+    }
+  },
+  methods: {
+    fetchWeather(e) {
+      if (e.key === 'Enter') {
+        fetch(`${this.urlBase}weather?q=${this.query}&units=metric&APPID=${this.apiKey}`)
+          .then(response => response.json())
+          .then(data => {
+            this.weather = data;
+          });
+      }
+    },
+  },
 }
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+#app {
+  background-image: url('./assets/cold-bg.jpg');
+  background-size: cover;
+  background-position: bottom;
+  transition: .4s;
+}
+
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, .25), rgba(0, 0, 0, .75));
+}
+
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border: none;
+  outline: none;
+  background: none;
+  background-color: rgba(255, 255, 255, .5);
+  border-radius: 0 16px 0 16px;
+  transition: .4s;
+  box-shadow: 0 0 8px rgba(0, 0, 0, .25);
+}
+
+.search-box .search-bar:focus {
+  background-color: rgba(255, 255, 255, .75);
+  box-shadow: 0 0 16px rgba(0, 0, 0, .25);
+  border-radius: 16px 0 16px 0;
+}
+
+.location-box .location {
+  color: #fff;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, .25);
+}
+
+.location-box .date {
+  color: #fff;
+  font-size: 20px;
+  font-weight: 300;
+  text-align: center;
+  font-style: italic;
+}
+
+.weather-box {
+  text-align: center;
+}
+
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #fff;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+  background-color: rgba(255, 255, 255, .25);
+  border-radius: 16px;
+  margin: 30px 0;
+  box-shadow: 3px 6px rgba(0, 0, 0, .25);
+}
+
+.weather-box .weather {
+  color: #fff;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, .25);
+}
 </style>
